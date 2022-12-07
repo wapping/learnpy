d3025b1dd889725b86aaa67a804286a285dc9485
wapping <warpin@163.com>
2022-12-07 17:45:48 +0800

    list

--- list_.py
+++ list_.py
@@ -56,67 +56,37 @@ def list_delete():
     print(ls)
 
 
-def for_iter_list():
+def list_search():
+    """在list中查找"""
     ls = [1, 2, 3]
     for i in ls:
         print(i, end='\t')
 
-
-def list_index():
     ls = [1, 2, 3, 3]
     print(ls.index(3))
     print(ls.index(4))
 
-
-def if_in_list():
-    ls = [1, 2, 3]
     if 4 in ls:
         print(True)
     else:
         print(False)
 
-
-def count():
-    ls = [1, 2, 3]
     print("Count 3 =", ls.count(3))
     print("Count 4 =", ls.count(4))
 
+    print(ls[0])
 
-def append():
-    ls = []
-    for i in range(10):
-        ls.append(i * 2)
-    print(ls)
-
-
-def remove():
-    ls = [1, 2, 3, 3]
-    print(ls)
-    ls.remove(3)
-    print(ls)
-
-
-def list_pop():
-    ls = [1, 2, 3]
-    print(ls)
-    ls.pop(1)
-    print(ls)
+    print(ls[0: 3])
 
 
 def list_sort():
+    """对list排序"""
     ls = [2, 3, 1]
     ls.sort()
     print(ls)
     ls.sort(reverse=True)
     print(ls)
-
+    
 
 if __name__ == '__main__':
     ...
-    # count()
-    # remove()
-    # list_index()
-    # list_sort()
-    # list_pop()
-    # list_add()
-    list_delete()
